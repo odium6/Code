beec1ef423aa637d1e27660461c22f72633ff39c
Максим <maksimmelehin480@gmail.com>
2021-11-23 20:01:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,30 @@ password = int(input('Enter code access\n'))
 languages = ['python', 'cpp', 'java', 'c#', 'javascript', 'go', 'pascal', 'basic']
 count = []
 succs = 0
+def point(number):
+    number = str(number)[::-1]
+    result = ''
+    for i, num in enumerate(number):
+        if i % 3 == 0:
+            result += '.'
+        result += num
+    result = result[::-1][:-1]
+    return result
+
 def api():
     for i in languages:
         try:
-            a = requests.get(f"https://api.github.com/search/repositories?q=language:{i}&sort=stars")
             data = a.json()
             dat = data['total_count']
-            print(Fore.RESET + f'{i} -- ' + Fore.GREEN + f'{dat}' + Fore.RESET + ' -- projects')
             count.append(data['total_count'])
             succs = 1
         except KeyError:
-            print(Fore.RESET + 'Server rejected request' + Fore.GREEN + f' {i}' + Fore.RED + ' (KeyError)')
             succs = 0
         except:
             print(Fore.RED + 'Unknown error')
             succs = 0
     if(succs == 1):
         time.sleep(1)
-        print(Fore.GREEN + "\nSUCCESSFUL!")
     else:
         pass
 
