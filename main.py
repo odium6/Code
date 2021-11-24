8ec53b91c17621f97426131358b5d9357e5aee62
Максим <maksimmelehin480@gmail.com>
2021-11-24 16:42:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import time
 from colorama import Fore, init
 
-number_c = 1
 errors = 0
 num = 0
 password = int(input('Enter code access\n'))
@@ -89,37 +88,6 @@ if(check_custom == 1):
 else:
     pass
 
-def point(number):
-    number = str(number)[::-1]
-    result = ''
-    for i, num in enumerate(number):
-        if i % 3 == 0:
-            result += '.'
-        result += num
-    result = result[::-1][:-1]
-    return result
-
-def api():
-    for i in languages:
-        try:
-            a = requests.get(f"https://api.github.com/search/repositories?q=language:{i}")
-            data = a.json()
-            dat = data['total_count']
-            print(Fore.RESET + f'{i} -- ' + Fore.GREEN + point(f'{dat}') + Fore.RESET + ' -- projects')
-            count.append(data['total_count'])
-            succs = 1
-        except KeyError:
-            print(Fore.RESET + 'Server ' + Fore.RED + 'rejected ' + Fore.RESET + 'request' + Fore.CYAN+ f' {i}' + Fore.RED + ' (KeyError)')
-            succs = 0
-        except:
-            print(Fore.RED + 'Unknown error')
-            succs = 0
-    if(succs == 1):
-        time.sleep(1)
-        print(Fore.LIGHTCYAN_EX + "\nSUCCESSFUL!")
-    else:
-        pass
-
 if(password == 1):
     print(Fore.RESET + 'Get info.', end='')
     time.sleep(1)
@@ -134,8 +102,6 @@ if(password == 1):
 if(password == 2):
     print(Fore.GREEN + "START!")
     api()
-if(password != 1) and (password != 2):
-    print(Fore.RED + 'YOU DON’T HAVE ACCESS')
 
 print(Fore.LIGHTRED_EX + f'\nTotal errors - {errors}')
 
